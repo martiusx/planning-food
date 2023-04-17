980997f89cdeea6bb43ed22af2ba390b1dd459cb
martiusx <Maciej.Cis@o2.pl>
2023-04-18 00:58:27 +0200

    created about-us section and added public assets

---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,50 @@
+const AboutUs = () => {
+  return (
+    <div className="about_us">
+      <div className="about_us__element">
+        <img
+          className="about_us__element__img"
+          src="assets/checkmark.png"
+          alt=""
+        ></img>
+        <h4>Lorem ipsum dolor sit amet</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+          reprehenderit accusamus ad? Doloribus accusantium minima libero odit
+          possimus, est delectus? Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quo, repudiandae.
+        </p>
+      </div>
+      <div className="about_us__element">
+        <img
+          className="about_us__element__img"
+          src="assets/clock.png"
+          alt=""
+        ></img>
+        <h4>Lorem ipsum dolor sit amet</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+          reprehenderit accusamus ad? Doloribus accusantium minima libero odit
+          possimus, est delectus? Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quo, repudiandae.
+        </p>
+      </div>
+      <div className="about_us__element">
+        <img
+          className="about_us__element__img"
+          src="assets/menu.png"
+          alt=""
+        ></img>
+        <h4>Lorem ipsum dolor sit amet</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis
+          reprehenderit accusamus ad? Doloribus accusantium minima libero odit
+          possimus, est delectus? Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quo, repudiandae.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,11 +1,13 @@
 import Carousel from "../components/Carousel";
 import CallToAction from "../components/CallToAction";
+import AboutUs from "../components/AboutUs";
 
 const Main = () => {
   return (
     <div className="main">
       <Carousel />
       <CallToAction />
+      <AboutUs />
     </div>
   );
 };
