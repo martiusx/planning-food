4dba58122c7791241fcebd1d7038079688d91fb0
martiusx <Maciej.Cis@o2.pl>
2023-04-23 17:49:36 +0200

    created footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Nav from "../layout/Nav";
 import Main from "../layout/Main";
+import Footer from "../layout/Footer";
 
 const Home = () => {
   return (
     <>
       <Nav />
       <Main />
+      <Footer />
     </>
   );
 };
