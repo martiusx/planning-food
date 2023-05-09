dfb51631a17ce4ada54aaefe9c487640b8b8289e
martiusx <Maciej.Cis@o2.pl>
2023-05-09 19:37:06 +0200

    checking the username from localhost and selecting the appropriate view

--- src/pages/AppHome.tsx
+++ src/pages/AppHome.tsx
@@ -1,14 +1,24 @@
 import Nav from "../layout/Nav";
 import AppSideBar from "../components/AppSideBar";
 import AppNewUser from "../components/AppNewUser";
+import { useEffect, useState } from "react";
 
 const AppHome = () => {
+  const [username, setUsername] = useState<string>("");
+
+  useEffect(() => {
+    const storedName = localStorage.getItem("username");
+    if (storedName) {
+      setUsername(storedName);
+    }
+  }, []);
+
   return (
     <>
       <Nav />
       <div className="app_home">
         <AppSideBar />
-        <AppNewUser />
+        {username ? <h1>{username}</h1> : <AppNewUser />}
       </div>
     </>
   );
