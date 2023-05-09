8fe1ff15574ea1fbf15a7b96a6a7709c65a1edd3
martiusx <Maciej.Cis@o2.pl>
2023-05-09 19:32:15 +0200

    added form validation, saving selected username to localhost

--- src/components/AppNewUser.tsx
+++ src/components/AppNewUser.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import Button from "./Button";
 
 const AppNewUser = () => {
+  const [username, setUsername] = useState<string>("");
+
+  const saveUsername = () => {
+    if (username && username != "") {
+      localStorage.setItem("username", username);
+    } else {
+      alert("wpisz swoje imię");
+    }
+  };
+
   return (
     <div className="app_new_user">
       <div className="app_new_user__wrapper">
         <div className="app_new_user__text">
           <h3>Witaj,</h3>
           <p>wygląda ma to że jesteś tutaj pierwszy raz!</p>
-          <form>
-            <input type="text" placeholder="tutaj wpisz jak masz na imię" />
+          <form onSubmit={saveUsername}>
+            <input
+              type="text"
+              placeholder="tutaj wpisz jak masz na imię"
+              onChange={(e) => setUsername(e.target.value)}
+              pattern="[a-zA-Z]+"
+            />
             <Button buttonText="Gotowe!" />
           </form>
           <p>
