d2bd5983b0ed48a08551a644c3e5757c48a4c12c
martiusx <Maciej.Cis@o2.pl>
2023-05-12 22:38:48 +0200

    added widget notification component - not completed

--- src/components/AppReturningUser.tsx
+++ src/components/AppReturningUser.tsx
@@ -1,10 +1,30 @@
 import AppWidgetAddItem from "./AppWidgetAddItem";
+import AppWidgetNotification from "./AppWidgetNotification";
+import closeIcon from "../assets/close-icon.png";
+import infoIcon from "../assets/info-icon.png";
+import checkIcon from "../assets/check-icon.png";
+import alerIcon from "../assets/alert-icon.png";
 
 const AppReturningUser = () => {
   return (
     <div className="app_returning_user">
       <AppWidgetAddItem widgetText="dodaj przepis" />
       <AppWidgetAddItem widgetText="dodaj plan" />
+      <AppWidgetNotification
+        notificationIcon={infoIcon}
+        closeIcon={closeIcon}
+        notificationMessage="masz już 99 przepisów, nieźle!"
+      />
+      <AppWidgetNotification
+        notificationIcon={checkIcon}
+        closeIcon={closeIcon}
+        notificationMessage="masz już 99 przepisów, nieźle!"
+      />
+      <AppWidgetNotification
+        notificationIcon={alerIcon}
+        closeIcon={closeIcon}
+        notificationMessage="masz już 99 przepisów, nieźle!"
+      />
     </div>
   );
 };
